f59bd015f20261757dcc48b2ab668266c59cf215
Sandeep Kumar <sandeep.kumar@transplace.com>
2022-01-01 21:07:38 -0600

    Intercept http reqeust

--- src/app/services/interceptor.service.ts
+++ src/app/services/interceptor.service.ts
@@ -1,9 +1,37 @@
+import { ConfigService } from './config.service';
+import { AccountService } from './account.service';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Stock } from './stocks.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class InterceptorService {
+export class InterceptorService implements HttpInterceptor{
 
-  constructor() { }
+  constructor(private accountService: AccountService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+      const request = req.clone();
+      request.headers.append('Accept', 'application/json');
+      return next.handle(request);
+      // .do(event => {
+      //   if (event instanceof HttpResponse && event.url === ConfigService.get('api')) {
+      //     const stocks = event.body as Array<Stock>;
+      //     stocks.forEach(stock => {
+      //       this.accountService.stocks.forEach(item => {
+      //           if (stock.symbol === item.symbol) {
+      //           item.price = stock.price;
+      //           item.change = ((stock.price * 100) - (item.cost * 100)) / 100;
+      //         }
+      //       });
+      //     });
+      //     this.accountService.calculateValue();
+
+      //     return stocks;
+      //   }
+      // });
+  }
+
 }
